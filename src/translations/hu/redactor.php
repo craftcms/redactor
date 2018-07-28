e5a249b7f465513fdf45c1480f673bb3ce2252fe
Ottó Radics <otto@webmenedzser.hu>
2018-07-28 19:23:56 +0200

    Hungarian translation

---
+++ src/translations/hu/redactor.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'Add head' => 'Fejléc hozzáadása',
+    'Delete column' => 'Oszlop törlése',
+    'Delete head' => 'Fejléc törlése',
+    'Delete row' => 'Sor törlése',
+    'Delete table' => 'Táblázat törlése',
+    'Fullscreen' => 'Teljes képernyő',
+    'Insert column left' => 'Oszlop beszúrása balra',
+    'Insert column right' => 'Oszlop beszúrása jobbra',
+    'Insert Page Break' => 'Oldaltörés beszúrása',
+    'Insert row above' => 'Sor beszúrása fölé',
+    'Insert row below' => 'Sor beszúrása alá',
+    'Insert table' => 'Táblázat beszúrása',
+    'Link to a category' => 'Hivatkozás egy kategóriára',
+    'Link to an entry' => 'Hivatkozás egy bejegyzésre',
+    'Table' => 'Táblázat',
+    'Video' => 'Videó',
+    'Video Embed Code or Youtube/Vimeo Link' => 'Videó beágyazó kód vagy Youtube/Vimeo link',
+];
