087c53c204a00fd93a10e2a9b277f30f3c364943
Benoît Rouleau <benoit.rouleau@icloud.com>
2019-02-25 10:26:59 -0500

    Rewrite migration to modify both project config and the DB

--- src/migrations/m190225_003922_split_cleanup_html_settings.php
+++ src/migrations/m190225_003922_split_cleanup_html_settings.php
@@ -4,10 +4,10 @@ namespace craft\redactor\migrations;
 
 use Craft;
 use craft\db\Migration;
-use craft\db\Query;
-use craft\db\Table;
 use craft\helpers\Json;
-use craft\helpers\ArrayHelper;
+use craft\services\Fields;
+use craft\services\Matrix;
+use craft\redactor\Field;
 
 /**
  * m190225_003922_split_cleanup_html_settings migration.
@@ -19,25 +19,50 @@ class m190225_003922_split_cleanup_html_settings extends Migration
      */
     public function safeUp()
     {
-        $fields = (new Query())
-            ->select(['id', 'settings'])
-            ->from([Table::FIELDS])
-            ->where(['type' => 'craft\\redactor\\Field'])
-            ->all($this->db);
+        $projectConfig = Craft::$app->getProjectConfig();
 
-        $fieldsService = Craft::$app->getFields();
+        // Don't make the same config changes twice
+        $schemaVersion = $projectConfig->get('plugins.redactor.schemaVersion', true);
+        if (version_compare($schemaVersion, '2.3.0', '>=')) {
+            return true;
+        }
 
-        foreach ($fields as $field) {
-            $settings = Json::decode($field['settings']);
-            if (is_array($settings) && array_key_exists('cleanupHtml', $settings)) {
-                $cleanupHtml = ArrayHelper::remove($settings, 'cleanupHtml');
-                $settings['removeInlineStyles'] = $cleanupHtml;
-                $settings['removeEmptyTags'] = $cleanupHtml;
-                $settings['removeNbsp'] = $cleanupHtml;
-                $this->update(Table::FIELDS, ['settings' => Json::encode($settings)], ['id' => $field['id']]);
-                $fieldsService->saveField($fieldsService->getFieldById($field['id']));
+        $projectConfig->muteEvents = true;
+
+        $fields = $projectConfig->get(Fields::CONFIG_FIELDS_KEY) ?? [];
+        foreach ($fields as $uid => &$field) {
+            if ($field['type'] === Field::class && isset($field['settings']) && is_array($field['settings'])) {
+                $cleanupHtml = $field['settings']['cleanupHtml'];
+                unset($field['settings']['cleanupHtml']);
+                $field['settings']['removeInlineStyles'] = $cleanupHtml;
+                $field['settings']['removeEmptyTags'] = $cleanupHtml;
+                $field['settings']['removeNbsp'] = $cleanupHtml;
+                $this->update('{{%fields}}', [
+                    'settings' => Json::encode($field['settings']),
+                ], ['uid' => $uid]);
+                $projectConfig->set(Fields::CONFIG_FIELDS_KEY . '.' . $uid, $field);
             }
         }
+
+        $matrixBlockTypes = $projectConfig->get(Matrix::CONFIG_BLOCKTYPE_KEY) ?? [];
+        foreach ($matrixBlockTypes as $matrixBlockTypeUid => &$matrixBlockType) {
+            $fields = $projectConfig->get(Matrix::CONFIG_BLOCKTYPE_KEY . '.' . $matrixBlockTypeUid . '.fields') ?? [];
+            foreach ($fields as $uid => &$field) {
+                if ($field['type'] === Field::class && isset($field['settings']) && is_array($field['settings'])) {
+                    $cleanupHtml = $field['settings']['cleanupHtml'];
+                    unset($field['settings']['cleanupHtml']);
+                    $field['settings']['removeInlineStyles'] = $cleanupHtml;
+                    $field['settings']['removeEmptyTags'] = $cleanupHtml;
+                    $field['settings']['removeNbsp'] = $cleanupHtml;
+                    $this->update('{{%fields}}', [
+                        'settings' => Json::encode($field['settings']),
+                    ], ['uid' => $uid]);
+                    $projectConfig->set(Matrix::CONFIG_BLOCKTYPE_KEY . '.' . $matrixBlockTypeUid . '.fields.' . $uid, $field);
+                }
+            }
+        }
+
+        $projectConfig->muteEvents = false;
     }
 
     /**
