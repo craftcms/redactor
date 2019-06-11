577ddf4ec53c04d78babacf99cb8dd98b8ac6184
Alain Rollmann <arollmann@gmail.com>
2019-06-11 13:51:52 +0200

    Add French translation

---
+++ src/translations/fr/redactor.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'Add head' => 'Insérer un en-tête de tableau',
+    'Delete column' => 'Supprimer la colonne',
+    'Delete head' => 'Supprimer l\'en-tête de tableau',
+    'Delete row' => 'Supprimer la ligne',
+    'Delete table' => 'Supprimer le tableau',
+    'Fullscreen' => 'Plein écran',
+    'Insert column left' => 'Insérer une colonne à gauche',
+    'Insert column right' => 'Insérer une colonne à droite',
+    'Insert Page Break' => 'Insérer un saut de page',
+    'Insert row above' => 'Insérer une ligne au-dessus',
+    'Insert row below' => 'Insérer une ligne en-dessous',
+    'Insert table' => 'Insérer un tableau',
+    'Link to a category' => 'Lien vers une catégorie',
+    'Link to an entry' => 'Lien vers une entrée',
+    'Table' => 'Tableau',
+    'Video' => 'Vidéo',
+    'Video Embed Code or Youtube/Vimeo Link' => 'Code d\'intégration d\'une vidéo ou lien Youtube/Vimeo',
+];
