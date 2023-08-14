e7c2de54ae6fd8868edec6b31590bfa5d187988f
Rainaldo Dijkman <rainaldo@appmanschap.nl>
2023-08-14 16:21:19 +0200

    added `link to asset translation`

--- src/translations/nl/redactor.php
+++ src/translations/nl/redactor.php
@@ -14,6 +14,7 @@ return [
     'Insert row below' => 'Rij hieronder invoegen',
     'Insert table' => 'Tabel invoegen',
     'Link to a category' => 'Link naar een categorie',
+	'link to an asset' => 'Link naar een bestand',
     'Link to an entry' => 'Link naar inhoud',
     'Table' => 'Tabel',
     'Video' => 'Video',
